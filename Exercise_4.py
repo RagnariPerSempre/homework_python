762d64a22b2f5a85728c36b5aafa32d636a72c62
RagnariPerSempre <94491208+RagnariPerSempre@users.noreply.github.com>
2022-05-28 13:22:31 +0500

    Change if

--- Exercise_4.py
+++ Exercise_4.py
@@ -5,7 +5,7 @@ lst = [3, 4, 56, 100, 15, 2, 20, 30]
 def multiplication_of_multiples(lst: lst):
     result = 1
     for i in lst:
-        if i % 3 == 0 or i % 5 == 0:
+        if i % 3 == 0 and i % 5 == 0:
             result *= i
     return result
 
